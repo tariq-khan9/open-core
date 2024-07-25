039c76e5e8e0d23352afc2d9eb00ddfc4c6dabab
tariq-khan9 <tariqkhan.cs9@gmail.com>
2024-07-25 15:15:53 +0500

    changes 1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
     <div className='navbar z-50'>   
         <div className=' flex flex-row '>
             <button className='bg-white p-[10px] border border-gray-300 pl-[12px] rounded-l-full'>
-                <img src='/images/logo.svg' className='w-6 h-6'/>
+                <img src='/images/logo.svg' alt='image' className='w-6 h-6'/>
             </button>
             <button className='bg-white p-2 pr-[10px]   border-y border-r border-gray-300  rounded-r-full'>
                  <HiBars3 size={25} color='primary'/>
